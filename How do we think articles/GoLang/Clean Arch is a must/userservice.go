5f165802ad461c826c07c32e35de8adfc59add50
suryatejabc <165936666+suryatejabc@users.noreply.github.com>
2024-04-03 23:10:33 +0530

    Create userservice.go

---
+++ How do we think articles/GoLang/Clean Arch is a must/userservice.go
@@ -0,0 +1,11 @@
+type UserService struct {
+    UserRepository IUserRepository
+}
+
+func NewUserService(repo IUserRepository) UserService {
+    return UserService{UserRepository: repo}
+}
+
+func (s *UserService) GetUserByID(id int64) (*User, error) {
+    return s.UserRepository.GetByID(id)
+}
